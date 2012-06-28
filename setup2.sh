12b57c0654410588527882eb74867ce44c83ff18
Jeff Haack <jeff.haack@gmail.com>
2012-06-28 11:55:26 +0400

    changes

--- setup2.sh
+++ setup2.sh
@@ -1,21 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 readonly USERNAME=jeff
 readonly USERPASS=pleh
 
-readonly POSTGRES_USER=db_user
-readonly POSTGRES_PASS=db_pass
-readonly POSTGRES_DB=db_name
+readonly POSTGRES_USER=blog
+readonly POSTGRES_PASS=blog_pass
+readonly POSTGRES_DB=blog_production
 
 readonly WEBUSER=nginx
 readonly WEBGROUP=web
 
 # Install requisite packages
+apt-get update
+apt-get -y install curl git-core python-software-properties
 add-apt-repository ppa:nginx/stable
 add-apt-repository ppa:pitti/postgresql
 add-apt-repository ppa:chris-lea/node.js
 apt-get update
-apt-get -y install curl git-core python-software-properties
 apt-get -y install nginx
 apt-get -y install postgresql libpq-dev
 apt-get -y install nodejs
@@ -42,22 +43,28 @@ sudo -u postgres psql -c "create database $POSTGRES_DB owner $POSTGRES_USER;"
 sudo bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 umask g+w
 source /etc/profile.d/rvm.sh
+usermod -a -G rvm $USERNAME # add USERNAME to rvm group
+
 # Install 1.9.3 and set as default
 rvm install 1.9.3
 source "/usr/local/rvm/scripts/rvm"
-rvm --default use 1.9.2
+rvm --default use 1.9.3
 
 # Install rails
-gem install rails
-gem install unicorn
+gem install rails --no-ri --no-rdoc
+gem install unicorn --no-ri --no-rdoc
 
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+cp config/nginx.conf /etc/nginx/
 service nginx start
 
 # Setup web directory
 mkdir /var/www
 chgrp -R web /var/www # set /var/www owner group to "web"
 chmod -R 775 /var/www # group write permission
-# usermod -a -G web USERNAME  # if you need to add user to web group
+usermod -a -G web $USERNAME  # if you need to add user to web group
+
+
 
 # To get the unicorn config file
 #curl -o config/unicorn.rb https://raw.github.com/defunkt/unicorn/master/examples/unicorn.conf.rb
